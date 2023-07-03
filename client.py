247049eab4822b8475d5d9f3113a79526f2751dd
Dan Haver <haverd08@gmail.com>
2023-07-03 20:46:33 +0100

    V1 first update

--- client.py
+++ client.py
@@ -1,19 +1,26 @@
 import socket
-import sys
 from keys import HOST, PORT
+
+HEADER = 64
+FORMAT = "utf-8"
+DISCONN_MSG = "!DISCONNECT"
 
 
 def main():
-    if len(sys.argv) != 2:
-        print("Usage: client.py <product_code>")
-        return
-    # create a socket object with the IP family IPv4 and using TCP
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        # connect to the global host and port
-        s.connect((HOST, PORT))
-        # send "Hello, world"
-        s.sendall(b"Hello, world")
-        # read the servers reply
-        data = s.recv(1024)
-    # print the servers reply
-    print(f"Received {data!r}")
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((HOST, PORT))
+    send_data("hello, world", client)
+    send_data(DISCONN_MSG, client)
+
+
+def send_data(data, client):
+    data = data.encode(FORMAT)
+    data_len = len(data)
+    send_length = str(data_len).encode(FORMAT)
+    send_length += b" " * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(data)
+
+
+if __name__ == "__main__":
+    main()
